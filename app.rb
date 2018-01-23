52b947e735f8746c114aa88b932ed4dafb265478
admin <isalechat@yahoo.com>
2018-01-23 15:52:49 +0100

    commit

---
+++ app.rb
@@ -0,0 +1,24 @@
+# Méthode 1
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+puts "Hi world"
+
+# Méthode 2
+puts "Hello World\n"*10
+
+# Méthode 3
+for i in 1..10
+  puts "Bonjour World !"
+end
+
+# Méthode 4
+10.times do
+  puts "Coucou le monde !"
+end
